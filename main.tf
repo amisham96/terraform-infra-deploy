ed810a4ccb965796a8ae38fc75e798013be2c750
root <root@jenkinsvm.dv4headfmdfuze2frvvilikdme.bx.internal.cloudapp.net>
2022-07-10 11:56:05 +0000

    commit

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "azurerm_kubernetes_cluster" "test" {
 
   default_node_pool {
     name       = "default"
-    node_count = 2
+    node_count = 1
     vm_size    = "Standard_DS2_v2"
   }
 
